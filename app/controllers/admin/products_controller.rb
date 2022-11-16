b2c228f7902a652d4b4785c6ba4acec94b94f0fc
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-16 23:21:02 +0000

    Move the sensitive info to the .env and refer to them with ENV; Add placeholders to these keys to the .env.example

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTHENTICATION_NAME"], password: ENV["HTTP_BASIC_AUTHENTICATION_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
