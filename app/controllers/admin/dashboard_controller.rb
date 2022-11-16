4ae6d4ce603ed4732d2c523c30005e6335ac80d4
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-16 23:54:21 +0000

    Set instance variables in the controller action with values

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTHENTICATION_NAME"], password: ENV["HTTP_BASIC_AUTHENTICATION_PASSWORD"]
 
   def show
+    @total_products = Product.sum(:quantity)
+    @total_categories = Category.count(:all)
   end
 end
