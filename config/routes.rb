176d9484ce6773e901c5687eaefe1e5a88d2341f
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-17 19:56:13 +0000

    Create resources  route for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
-  resource :users, only: [:new, :create]
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
     root to: 'dashboard#show'
