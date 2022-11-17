5dcbeee7b5b5e81f73baa2860f6947dbed6ac3ab
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-17 19:20:31 +0000

    Implement the create action and add a private method called user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,26 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(product_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to users_new_path
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :passoword_confirmation
+    )
+  end
+  
 end
