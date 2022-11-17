84c845ebc4fe03d44a3d955246ac181dca952337
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-17 21:21:28 +0000

    Refactor some codes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email])
 
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to root_path
     else
-      redirect_to 'sessions/new'
+      redirect_to new_session_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to new_session_path
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      session[:user_id] = @user.id
+    if user.save
+      session[:user_id] = user.id
       redirect_to root_path
     else
-      redirect_to '/users/new'
+      redirect_to new_user_path
     end
   end
 
@@ -22,7 +21,7 @@ class UsersController < ApplicationController
       :last_name,
       :email,
       :password,
-      :passoword_confirmation
+      :password_confirmation
     )
   end
 
