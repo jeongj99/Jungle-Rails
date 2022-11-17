64fa1ff4887ff01e73a473507dde80bfa8c948f2
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-17 01:21:19 +0000

    Add a link_to to the admin categories index page in the top nav

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,6 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTHENTICATION_NAME"], password: ENV["HTTP_BASIC_AUTHENTICATION_PASSWORD"]
 
   def index
-    @categories = Categories.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 end
