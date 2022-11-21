b792f40e00fcaba123360a928af03e3d6c29c893
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-21 21:03:15 +0000

    Implement test for last_name validation and update user model to make the test pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,17 @@ RSpec.describe User, type: :model do
     before :each do
       @user = User.new(first_name: "Alex", last_name: "Jeong", email: "asdf@gmail.com", password: "password", password_confirmation: "password")
     end
+    
     it "should have a first_name validation error within the .errors.full_messages array when first_name is set to nil" do
       @user.first_name = nil
       @user.valid?
       expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it "should have a last_name validation error within the .errors.full_messages array when last_name is set to nil" do
+      @user.last_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
   end
 end
