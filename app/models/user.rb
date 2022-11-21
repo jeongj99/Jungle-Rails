9bb2a1d6fa4d770757c3f8a29a70b2bac23d67b4
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-21 21:45:24 +0000

    Create a test that authenticate_with_credentials returns nil if authentication fails and create that class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before :each do
+    @user = User.new(first_name: "Alex", last_name: "Jeong", email: "asdf@gmail.com", password: "password", password_confirmation: "password")
+  end
+
   describe "Validations" do
-    before :each do
-      @user = User.new(first_name: "Alex", last_name: "Jeong", email: "asdf@gmail.com", password: "password", password_confirmation: "password")
-    end
-    
     it "should have a first_name validation error within the .errors.full_messages array when first_name is set to nil" do
       @user.first_name = nil
       @user.valid?
@@ -54,6 +54,11 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = "pw"
       @user.valid?
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+    end
+  end
+
+  describe ".authenticate_with_credentials" do
+    it "should return nil if authentication fails" do
     end
   end
 end
