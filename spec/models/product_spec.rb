f7b1367379a593c6b7c74e80cd8abefb1fdd9812
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-21 19:44:14 +0000

    Implement test that ensures that a product is saved with all four fields set; implement validation test when name is nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    before :each do
+      @category = Category.new
+    end
+
+    it "saves successfully if a product has all four fields set" do
+      params = {
+        name: 'Cactus',
+        price: 1200,
+        quantity: 14,
+        category: @category
+      }
+      @product = Product.new(params)
+      @product.save!
+      expect(@product.id).to be_present
+    end
+
+    it "should have a name validation error within the .errors.full_messages array" do
+      params = {
+        name: nil,
+        price: 1200,
+        quantity: 14,
+        category: @category
+      }
+      @product = Product.new(params)
+      @product.save
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
