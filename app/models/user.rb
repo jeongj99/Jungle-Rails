749adb7c2e0d5fd5d325021b6c745af894fd6db0
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-21 20:54:55 +0000

    Implement test for first_name validation and update user model to make the test pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "Validations" do
+    before :each do
+      @user = User.new(first_name: "Alex", last_name: "Jeong", email: "asdf@gmail.com", password: "password", password_confirmation: "password")
+    end
+    it "should have a first_name validation error within the .errors.full_messages array when first_name is set to nil" do
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
   end
 end
