dfe828ed6bf7ad1f7fd6f5ff7faf5a920bea04e7
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-23 01:11:12 +0000

    Add register test

---
+++ cypress/integration/user-login.spec.js
@@ -0,0 +1,31 @@
+describe("Sign up and log in", () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it("Should be able to register and once registered, it should take them to the home page with the nav bar displaying logout", () => {
+    cy.get("[data-test-id=register-link]").click();
+
+    cy.get("#user_first_name").type("Obiwan");
+
+    cy.get("#user_last_name").type("Kenobi");
+
+    cy.get("#user_email").type("abcd@gmail.com");
+
+    cy.get("#user_password").type("password");
+
+    cy.get("#user_password_confirmation").type("password");
+
+    // cy.contains(".btn", "Register").click();
+
+    cy.get("form").submit();
+
+    cy.contains("li", "Signed in as Obiwan Kenobi");
+
+    cy.contains("li", "Logout");
+  });
+
+  it("Should be able to log in and once logged in, it should take them to the home page with the nav bar displaying logout", () => {
+    cy.get("[data-test-id=login-link]").click();
+  })
+});
